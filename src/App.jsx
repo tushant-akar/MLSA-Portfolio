527184b321cbd9c1aec051fe4605ea4aab61ff11
Tushant Akar <106074556+tushant-akar@users.noreply.github.com>
2024-01-06 18:47:09 +0000

    set homepage for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Tushant Akar",
+  title: "Android Devloper & Blockchain Enthusiast",
+  email: "tushantakar10@gmail.com",
+  gitHub: "https://www.github.com/tushant-akar",
+  instagram: "https://www.instagram.com/tushant_akar",
+  linkedIn: "https://www.linkedin.com/in/tushant-akar",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://www.twitter.com/tushant_akar",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Android Devloper studying at Indian Institute of Information Technology Vadodara. I enjoy creating unique and simplistic apps for daily life uses. I am also a Blockchain Enthusiast and have worked on various projects related to Blockchain.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Android Development",
+  "Blockchain",
+  "Firebase",
+  "HTML5",
+  "Teamwork",
+  "Leadership",
+  "Communication",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Android Development experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
